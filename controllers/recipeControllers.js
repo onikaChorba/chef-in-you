c05609a33ed13c4085e96500a6f3717948d5a521
Onika Chorba <110539259+onikaChorba@users.noreply.github.com>
2024-09-12 12:05:53 +0300

    feat: getAll and getOne

--- controllers/recipeControllers.js
+++ controllers/recipeControllers.js
@@ -1,4 +1,39 @@
 import RecipeModel from "../models/recipe.js";
+
+export const getAll = async (req, res) => {
+  try {
+    const recipes = await RecipeModel.find().populate("user").exec();
+    res.json(recipes);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Recipes are not found",
+    });
+  }
+};
+
+export const getOne = async (req, res) => {
+  try {
+    const recipeId = req.params.id;
+    const recipe = await RecipeModel.findOneAndUpdate(
+      { _id: recipeId },
+      { $inc: { viewsCount: 1 } },
+      { returnDocument: "after" }
+    ).exec();
+
+    if (!recipe) {
+      return res.status(404).json({ message: "Recipe is not found" });
+    }
+
+    res.json(recipe);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Recipes are not found",
+    });
+  }
+};
+
 export const create = async (req, res) => {
   try {
     const doc = new RecipeModel({
